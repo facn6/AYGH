cedf5e5d90b7a8841d7e721cee36588e6df059ec
ayah <ayah_kh5@hotmail.com>
2019-07-31 12:46:06 +0300

    server changes

--- src/servers/router.js
+++ src/servers/router.js
@@ -1,1 +1,22 @@
 
+const handler = require("./handlers");
+
+const router = (request, response) => {
+const url = request.url;
+  if (url === "/") {
+   handler.handleHome(request, response);
+   }
+   // else if(url==="/json") {
+   // handler.handleJson(request, response);
+   // }
+   else if (url.indexOf(".") !== -1) {
+   handler.handlePublic(request, response);
+  }
+   else
+   {
+      handler.handleNotfound(request,response);
+   }
+ }
+
+
+ module.exports = router ;
