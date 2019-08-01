c68651cd8151960fdf2726d827377106c3a25f95
ayah <ayah_kh5@hotmail.com>
2019-08-01 09:38:12 +0300

    api

--- public/view/main.js
+++ public/view/main.js
@@ -1,5 +1,34 @@
-const button = document.getElementbyID('button');
-const input = document.getElementbyID('inputbar').value;
-button.addEventListener("click", call(input) => {
-    
-})
+
+
+function getSelectValue()
+{
+    var selectedValue = document.getElementById("list").value;
+    fetch(`/${selectedValue}`).then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        var result = document.getElementById('result1');
+        while (result.hasChildNodes()) {
+            result.removeChild(result.firstChild);
+        }
+
+
+        for(let i=0;i<data[sources].length;i++){
+        var card=document.createElement('div');
+        card.setAttribute('class','card');
+        card.setAttribute('id',i);
+
+
+        var label = document.createElement('label');
+        label.innerHTML = data.sources[i].name;
+
+        card.appendChild(label);
+        var link = document.createElement('h');
+        link.href = data.sources[i].url;
+        link.innerHTML ="Check the channel's website";
+        card.appendChild(link);
+
+        result.appendChild(card);
+    }
+});
+
+}
